46225c2a8cb9fb1df1b2df2455b017a378d8a4e1
ygxxd <764863366@qq.com>
2026-09-15 16:09:08 +0800

    update docs styles

--- src/components/mark_down.rs
+++ src/components/mark_down.rs
@@ -3,18 +3,18 @@ use pulldown_cmark::{html, Options, Parser};
 
 #[component]
 pub fn MarkDown(content: Option<String>) -> Element {
-    let inner_html = if let Some(content) = content {
+    if let Some(content) = content {
         let parser = Parser::new_ext(content.as_str(), Options::all());
         let mut html_output = String::new();
         html::push_html(&mut html_output, parser);
-        html_output
-    } else {
-        "".to_string()
-    };
 
-    rsx! {
-        div {
-            dangerous_inner_html: inner_html
+        rsx! {
+            div {
+                class: "markdown-body",
+                dangerous_inner_html: html_output
+            }
         }
+    } else {
+        rsx! {}
     }
 }
